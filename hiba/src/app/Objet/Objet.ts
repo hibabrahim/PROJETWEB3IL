ca68d6cd82ec233b31b1d1fe03311fb0eb2c31d4
hibabrahim <hiba.dhafer@esprit.tn>
2025-09-17 23:58:28 +0200

    Initial commit

---
+++ hiba/src/app/Objet/Objet.ts
@@ -0,0 +1,14 @@
+export class Objet {
+  id: number;
+  etat: string;
+  description: string;
+ 
+
+  constructor(id: number, etat: string, description: string) {
+    this.id = id;
+    this.etat = etat;
+    this.description = description;
+
+
+  
+}}
